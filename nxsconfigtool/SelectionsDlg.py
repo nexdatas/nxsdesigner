defb2fae4873583c4e5324a052b3cf6ca4b73a3c
Jan Kotanski <jankotan@gmail.com>
2025-11-21 15:04:35 +0100

    update selections

--- nxsconfigtool/SelectionsDlg.py
+++ nxsconfigtool/SelectionsDlg.py
@@ -77,21 +77,24 @@ class SelectionsDlg(QDialog):
     #  creates GUI
     # \brief It calls setupUi and  connects signals and slots
     def createGUI(self):
-
+        self.ui.setupUi(self)
         if self.keytype.lower() == "slices":
             self.keytype = "slices"
-            self.ui.keytypeComboBox.setValue(0)
+            self.ui.keytypeComboBox.setCurrentIndex(0)
         else:
             self.keytype = "slab"
-            self.ui.keytypeComboBox.setValue(1)
+            self.ui.keytypeComboBox.setCurrentIndex(1)
         try:
             if self.rank is not None and int(self.rank) >= 0:
                 self.rank = int(self.rank)
                 for elem in [self.starts, self.stops, self.steps, self.offsets,
                              self.blocks, self.counts, self.strides]:
                     for i, ln in enumerate(elem):
-                        if ln:
-                            if '$var.' not in ln and '$datasources.' not in ln:
+                        if ln is not None:
+                            if isinstance(ln, int) or \
+                                   '$var.' not in ln and \
+                                   '$datasources.' not in ln:
+
                                 iln = int(ln)
                                 elem[i] = ln
                                 if iln < 1:
@@ -101,6 +104,7 @@ class SelectionsDlg(QDialog):
                         else:
                             elem[i] = None
         except Exception:
+            #  print(str(e))
             self.rank = 1
             self.starts = []
             self.stops = []
@@ -125,67 +129,81 @@ class SelectionsDlg(QDialog):
         if not self.strides:
             self.strides = []
 
-        self.ui.setupUi(self)
-
         self.ui.rankSpinBox.setValue(self.rank)
-
 
         self.ui.selTableWidget.itemChanged.connect(
             self.__tableItemChanged)
 
         self.ui.selTableWidget.setSortingEnabled(False)
-        self.__populateLengths()
+        self.__populateKeys()
         self.ui.rankSpinBox.setFocus()
 
         self.ui.rankSpinBox.valueChanged[int].connect(self.__valueChanged)
+        self.ui.keytypeComboBox.currentIndexChanged[str].connect(
+            self._currentIndexChanged)
+
+    # calls updateUi when the name text is changing
+    # \param text the edited text
+    def _currentIndexChanged(self, text):
+        if text == 'slices':
+            self.keytype = 'slices'
+        else:
+            self.keytype = 'slabs'
+        self.__populateKeys()
 
     # takes a name of the current dim
     # \returns name of the current dim
     def __currentTableDim(self):
-        return self.ui.selTableWidget.currentRow()
+        return (self.ui.selTableWidget.currentRow(),
+                self.ui.selTableWidget.currentColumn())
 
     # changes the current value of the dim
     # \brief It changes the current value of the dim
     # and informs the user about wrong values
     def __tableItemChanged(self, item):
-        row = self.__currentTableDim()
+        row, col = self.__currentTableDim()
 
-        if row not in range(len(self.lengths)):
+        if self.keytype.lower() == 'slices':
+            elems = [self.starts, self.stops, self.steps]
+        else:
+            elems = [self.offsets, self.blocks,
+                     self.counts, self.strides]
+
+        if col not in range(len(elems)):
             return
-        column = self.ui.selTableWidget.currentColumn()
-        if column == 0:
-            try:
-                if item.text():
-                    # if '$var' not in str(item.text()) and  \
-                    #         '$datasources' not in str(item.text()):
-                    #     iln = int(item.text())
-                    #     if iln < 1:
-                    #         raise ValueError("Non-positive length value")
-                    ln = str(item.text())
-                    self.lengths[row] = ln
-                else:
-                    self.lengths[row] = None
-            except Exception:
-                QMessageBox.warning(
-                    self, "Value Error", "Wrong value of the edited length")
+        elem = elems[col]
+        if row not in range(len(elem)):
+            return
+        try:
+            if item.text():
+                if '$var' not in str(item.text()) and  \
+                        '$datasources' not in str(item.text()):
+                    iln = int(item.text())
+                    if iln < 0:
+                        raise ValueError("Negative value")
+                ln = str(item.text())
+                elem[row] = ln
+            else:
+                elem[row] = None
+        except Exception:
+            # print(str(e))
+            QMessageBox.warning(
+                self, "Value Error", "Wrong value of the edited length")
 
-        self.__populateLengths()
+        self.__populateKeys()
 
     # calls updateUi when the name text is changing
     # \param text the edited text
     def __valueChanged(self):
         self.rank = int(self.ui.rankSpinBox.value())
-        self.__populateLengths(self.rank - 1)
+        self.__populateKeys(self.rank - 1)
 
     # fills in the dim table
     # \param selectedDim selected dim
-    def __populateLengths(self, selectedDim=None):
+    def __populateKeys(self, selectedDim=None):
         selected = None
         self.ui.selTableWidget.clear()
         self.ui.selTableWidget.setRowCount(self.rank)
-
-        while self.rank > len(self.lengths):
-            self.lengths.append(None)
 
         if self.keytype.lower() == 'slices':
             headers = ["Start", "Stop", "Step"]
@@ -201,22 +219,25 @@ class SelectionsDlg(QDialog):
             elems = [self.offsets, self.blocks,
                      self.counts, self.strides]
 
-        for elem in  elems:
+        for col, elem in enumerate(elems):
+            while self.rank > len(elem):
+                elem.append(None)
             for row, ln in enumerate(elem):
                 if ln:
                     item = QTableWidgetItem(unicode(ln))
                 else:
                     item = QTableWidgetItem("")
+                print(row, col, ln)
                 item.setData(Qt.UserRole, (long(row)))
                 if selectedDim is not None and selectedDim == row:
                     selected = item
-                self.ui.selTableWidget.setItem(row, 0, item)
-            self.ui.selTableWidget.resizeColumnsToContents()
-            self.ui.selTableWidget.horizontalHeader().setStretchLastSection(True)
-            if selected is not None:
-                selected.setSelected(True)
-                self.ui.selTableWidget.setCurrentItem(selected)
-                self.ui.selTableWidget.editItem(selected)
+                self.ui.selTableWidget.setItem(row, col, item)
+        self.ui.selTableWidget.resizeColumnsToContents()
+        self.ui.selTableWidget.horizontalHeader().setStretchLastSection(True)
+        if selected is not None:
+            selected.setSelected(True)
+            self.ui.selTableWidget.setCurrentItem(selected)
+            self.ui.selTableWidget.editItem(selected)
 
     # accepts input text strings
     # \brief It copies the selections name and type from lineEdit widgets
@@ -228,7 +249,7 @@ class SelectionsDlg(QDialog):
             elems = [self.offsets, self.blocks,
                      self.counts, self.strides]
 
-        for elem in  elems:
+        for elem in elems:
             while len(elem) > self.rank:
                 elem.pop()
         QDialog.accept(self)
@@ -245,9 +266,15 @@ if __name__ == "__main__":
     # selections form
     form = SelectionsDlg()
     form.rank = 2
-    form.keytype = "slides"
-    form.lengths = [[2,10, None], [None,20, 2]]
-#    form.lengths = [None, 3]
+    form.keytype = "slices"
+    form.starts = [2, None]
+    form.stops = [10, 20]
+    form.steps = [None, 2]
+    # form.keytype = "slabs"
+    form.offsets = [2, None]
+    form.blocks = [10, 20]
+    form.counts = [None, 2]
+    form.strides = [None, 21]
     form.createGUI()
     form.show()
     app.exec_()
@@ -255,7 +282,17 @@ if __name__ == "__main__":
     if form.result():
         if form.rank:
             logger.info("Selections: rank = %s" % (form.rank))
-        if form.lengths:
-            logger.info("Lengths:")
-            for mrow, mln in enumerate(form.lengths):
-                logger.info(" %s: %s " % (mrow + 1, mln))
+        if form.keytype:
+            logger.info("Selections: keytype = %s" % (form.keytype))
+
+        logger.info("Selections: starts = %s" % (form.starts))
+        logger.info("Selections: stops = %s" % (form.stops))
+        logger.info("Selections: steps = %s" % (form.steps))
+        logger.info("Selections: offsets = %s" % (form.offsets))
+        logger.info("Selections: blocks = %s" % (form.blocks))
+        logger.info("Selections: counts = %s" % (form.counts))
+        logger.info("Selections: strides = %s" % (form.strides))
+
+        #     logger.info("Lengths:")
+        #     for mrow, mln in enumerate(form.lengths):
+        #         logger.info(" %s: %s " % (mrow + 1, mln))
