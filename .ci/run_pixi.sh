9838256fa180fa407fedb739bce1ed908bf33031
Jan Kotanski <jankotan@gmail.com>
2026-09-08 15:19:50 +0200

    add printout

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -7,7 +7,7 @@ echo "TEST1"
 docker exec  ndts /bin/bash -c 'pwd'
 docker exec  ndts /bin/bash -c 'source .sh.sh'
 docker exec  ndts /bin/bash -c 'ls -ltr '
-docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi add  numpy pytango  setuptools pip wheel argcomplete lxml pytz pyyaml pytango python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver pymysql nxstools qt-main pyqt>=5.15,<6 '
+docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi add  numpy pytango  setuptools pip wheel argcomplete lxml pytz pyyaml pytango python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver pymysql nxstools pip qt-main "pyqt>=5.15,<6" '
 
 echo "run nxsdesigner tests"
 docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation ;  python test '
