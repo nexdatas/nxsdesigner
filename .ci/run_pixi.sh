eb5092889e7aca0e93e93efa30f5e57dbdf2f671
Jan Kotanski <jankotan@gmail.com>
2026-09-08 14:54:23 +0200

    fix

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 echo "install pixi"
-docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh ; source .sh.sh ; pixi add   numpy pytango  setuptools pip wheel argcomplete lxml pytz pyyaml pytango python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver  pip pymysql nxstools qt-main pyqt>=5.15,<6 '
+docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh '
+echo "TEST1"
+docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi add   numpy pytango  setuptools pip wheel argcomplete lxml pytz pyyaml pytango python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver  pip pymysql nxstools qt-main pyqt>=5.15,<6 '
 
 echo "run nxsdesigner tests"
 docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation ;  python test '
