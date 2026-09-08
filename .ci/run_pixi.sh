9a0f63ab247402f920e50ff550f2c004be6e501e
Jan Kotanski <jankotan@gmail.com>
2026-09-08 15:25:48 +0200

    set QT_QPA_PLATFORM=offscreen for tests

--- .ci/run_pixi.sh
+++ .ci/run_pixi.sh
@@ -2,15 +2,10 @@
 
 echo "install pixi"
 docker exec  ndts /bin/bash -c 'curl -fsSL https://pixi.sh/install.sh | sh ; export PATH=/var/lib/tango/.pixi/bin:$PATH ; cp .github/workflows/pixi/pixi.toml . ; pixi shell-hook  > .sh.sh '
-
-echo "TEST1"
-docker exec  ndts /bin/bash -c 'pwd'
-docker exec  ndts /bin/bash -c 'source .sh.sh'
-docker exec  ndts /bin/bash -c 'ls -ltr '
 docker exec  ndts /bin/bash -c 'source .sh.sh ; pixi add  numpy pytango  setuptools pip wheel argcomplete lxml pytz pyyaml pytango python-dateutil pninexus fabio h5py matplotlib-base blissdata pytest docutils nxsconfigserver pymysql nxstools pip qt-main "pyqt>=5.15,<6" '
 
 echo "run nxsdesigner tests"
-docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation ;  python test '
+docker exec  ndts /bin/bash -c 'source .sh.sh ;  echo "export MYTANGO_PREFIX=$CONDA_PREFIX/bin" > /home/tango/.env ;   python -m pip install . -vv --no-deps --no-build-isolation ;  QT_QPA_PLATFORM=offscreen  python test '
 
 ERROR=$?
 if [ $ERROR -ne "0" ]
